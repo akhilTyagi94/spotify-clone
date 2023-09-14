a875921d80dea38c7d6001dba19d69e8fa3d7f11
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-09-14 10:28:01 +0530

    update

--- components/Loader.js
+++ components/Loader.js
@@ -1,4 +1,3 @@
-import { ThreeBounce } from "better-react-spinkit";
 import Image from "next/image";
 
 function Loader() {
@@ -8,12 +7,12 @@ function Loader() {
         <span className="relative w-[400px] h-[250px] lg:w-[550px] lg:h-[240px]">
           <Image
             src="https://rb.gy/y9mwtb"
+            alt="spotify"
             layout="fill"
             objectFit="contain"
             className="animate-pulse"
           />
         </span>
-        <ThreeBounce size={23} color="#15883e" />
       </div>
     </div>
   );
